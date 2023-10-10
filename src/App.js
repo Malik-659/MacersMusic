07afd587c9f552cd60fe07612b3210ecdf3b4f06
K1ndoctop <argen.paiysbekov04@gmail.com>
2023-10-10 19:39:11 +0600

    SideBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import SideBar from "./components/SideBar/SideBar";
 
 function App() {
-  return <div className="App"></div>;
+  return(
+    <>
+      <SideBar/>
+    </>
+    ) 
 }
 
 export default App;
