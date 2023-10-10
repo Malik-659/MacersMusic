ad425bb303a871a8057c1acaf5258210171b0b4b
Ваше имя <sulaimanmind862@gmail.com>
2023-10-10 22:57:31 +0600

    navbar+sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -4,26 +4,51 @@ import Home from "../../images/Home.svg";
 import Playlist from "../../images/playlist.svg";
 import Radio from "../../images/radio.svg";
 import Video from "../../images/videos.svg";
+import searchlogo from "../../images/search.svg";
 import Profile from "../../images/profile.svg";
 import Logout from "../../images/Logout.svg";
 import { useNavigate } from "react-router-dom";
 
 const SideBar = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
-    <div className="pl-7 pt-6 h-full bg-[#1D2123] ">
-      <div className="pl-3 pt-6">
+    <div className="pl-7 pt-4 h-full bg-[#1D2123] ">
+      <div className="pl-3 pt-4 flex items-center">
         <img src={logo} alt="" />
+        <img src={searchlogo} alt="navlogo" className="pl-20" />
+        <input
+          type="text"
+          placeholder="   Search"
+          className="bg-inherit w-3/4 ml-8 white-[#FFFFFF40] opacity-75 h-8 "
+        />
       </div>
       <div className="w-14 h-56 bg-[#1A1E1F] mt-10 mb-5 flex flex-col justify-around items-center rounded-3xl p-3">
-        <img src={Home} alt="" width="32" height="32" onClick={() => navigate('/')} />
-        <img src={Playlist} alt="" width="32" height="32" onClick={() => navigate('/playlist')}/>
-        <img src={Radio} alt=""  width="32" height="32" onClick={() => navigate('/radio')}/>
-        <img src={Video} alt=""  width="32" height="32"/>
+        <img
+          src={Home}
+          alt=""
+          width="32"
+          height="32"
+          onClick={() => navigate("/")}
+        />
+        <img
+          src={Playlist}
+          alt=""
+          width="32"
+          height="32"
+          onClick={() => navigate("/playlist")}
+        />
+        <img
+          src={Radio}
+          alt=""
+          width="32"
+          height="32"
+          onClick={() => navigate("/radio")}
+        />
+        <img src={Video} alt="" width="32" height="32" />
       </div>
       <div className="w-14 h-32 bg-[#1A1E1F] flex flex-col justify-around p-2 items-center rounded-3xl">
-        <img src={Profile} alt=""  width="32" height="32" />
-        <img src={Logout} alt=""  width="32" height="32" />
+        <img src={Profile} alt="" width="32" height="32" />
+        <img src={Logout} alt="" width="32" height="32" />
       </div>
     </div>
   );
