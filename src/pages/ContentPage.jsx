8baa25d44e0415ec21ab91ecf9005beb539fbe92
kjbuhyvbhxhcbiusgrelgmkef <malik.vundiz@gmail.com>
2023-10-10 18:12:02 +0600

    f

---
+++ src/pages/ContentPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Content from "../components/content/Content";
+
+const ContentPage = () => {
+  return (
+    <div>
+      <Content />
+    </div>
+  );
+};
+
+export default ContentPage;
