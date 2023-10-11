ed2aa4356f70f9616a4bdbdd6c8bc93e6a2af82e
Ваше имя <sulaimanmind862@gmail.com>
2023-10-11 16:54:17 +0600

    first

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -13,16 +13,16 @@ const SideBar = () => {
   const navigate = useNavigate();
   return (
     <>
-      <div className="pl-3 pt-8 pb-8 flex items-center bg-[#1D2123]">
+      <div className="fixed pl-3 pt-8 pb-8 flex items-center bg-[#1D2123] w-full z-20">
         <img src={logo} alt="" className="pl-8 " />
-        <BiSearch className="w-7 h-7 ml-12 fill-[#bababa]" />
+        <BiSearch className="w-7 h-7 ml-20 fill-[#bababa]" />
         <input
           type="text"
           placeholder="Search"
-          className="bg-inherit w-1/2 ml-3 text-white opacity-75 h-8 "
+          className="bg-inherit w-3/4 ml-8 text-white opacity-75 h-8 "
         />
       </div>
-      <div className="inline-block bg-[#1D2123] h-screen">
+      <div className="fixed top-24 inline-block bg-[#1D2123] h-screen">
 
         <div className="bg-[#1D2123] pl-8 pr-8">
           <div className="w-14 h-56 bg-[#1A1E1F] mt-10 mb-5 flex flex-col justify-around items-center rounded-3xl p-3 ">
