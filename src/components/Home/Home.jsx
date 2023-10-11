06cbfcef20fa5eaffd440e69002afc8a352fa819
Ваше имя <sulaimanmind862@gmail.com>
2023-10-11 13:12:20 +0600

    first

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='inline-block bg-[#1D2123] w-5/6 mt-0 pt-0 mt-0'>
-      
+    <div className='inline-block bg-[#1D2123] w-5/6'>
+      <div className="">kmnjn</div>
       duli
+      <ul>
+        <li className="d">ddddd</li>
+        <li className="d">ddddd</li>
+        <li className="d">ddddd</li>
+        <li className="d">ddddd</li>
+        <li className="d">ddddd</li>
+        <li className="d">ddddd</li>
+        <li className="d">ddddd</li>
+        <li className="d">ddddd</li>
+        <li className="d">ddddd</li>
+        <li className="d">ddddd</li>
+      </ul>
     </div>
   )
 }
