8a62ef105876b20f71c3c4a63dacb926c202350c
Ваше имя <sulaimanmind862@gmail.com>
2023-10-15 20:20:07 +0600

    hcbeh

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import Navbar from "./components/headers/Headers";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App"><Navbar /></div>;
 }
 
 export default App;
